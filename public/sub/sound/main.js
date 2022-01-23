9918678c781a32712fdf1a4979ac58757bc5ec9c
Arttu Pennanen <arttu@pennanen.org>
2022-01-23 22:02:21 +0200

    voice visualizer

--- public/sub/sound/main.js
+++ public/sub/sound/main.js
@@ -1,20 +1,86 @@
 const gapEl = document.getElementById('gap')
-const amountEl = document.getElementById('amount')
+const layersEl = document.getElementById('amount')
 const startEl = document.getElementById('start')
+
+const canvas = document.getElementById('visualizer')
+canvas.width = canvas.offsetWidth
+canvas.height = canvas.offsetHeight
+const context = canvas.getContext('2d')
+
+function clampBetween(input, min, max) {
+    return input < min ? min : input > max ? max : input
+}
+
+function mapBetween(current, in_min, in_max, out_min, out_max) {
+    const mapped = ((current - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    return clampBetween(mapped, out_min, out_max)
+}
+
+let start = 160,
+    amount = 10,
+    gap = 80,
+    polyphony = 3
+
+const resizeObserver = new ResizeObserver((entries) => {
+    const { width, height } = entries[0].contentRect
+    canvas.width = width
+    canvas.height = height
+
+    console.log('Size changed')
+})
+
+resizeObserver.observe(canvas)
 
 const audioContext = new AudioContext({ sampleRate: 44100 })
 const sampleRate = audioContext.sampleRate
+
+const analyser = audioContext.createAnalyser()
+analyser.fftSize = 2048
+
 const gainNode = audioContext.createGain()
 gainNode.gain.value = 0.1
 gainNode.connect(audioContext.destination)
+gainNode.connect(analyser)
 
 let sources = []
+let additionalSource
+
+let data = new Uint8Array(analyser.frequencyBinCount)
+
+function draw(data) {
+    context.clearRect(0, 0, canvas.width, canvas.height) //x,y,width,height
+    let startValue = Math.floor(mapBetween(Math.max(start - 2 * gap, 0), 0, 22000, 0, data.length - 1))
+    let endValue = Math.floor(mapBetween(Math.min(start + amount * gap + 2 * gap, 22000), 0, 22000, 0, data.length - 1))
+    let space = canvas.width / (endValue - startValue)
+    context.beginPath()
+    context.moveTo(0, canvas.height)
+    let line = 1
+    for (let i = startValue; i < endValue; i++) {
+        const value = data[i]
+        context.lineTo(space * line, canvas.height - value)
+        line++
+    }
+    context.lineTo(canvas.width, canvas.height)
+    context.stroke()
+}
+
+function animation() {
+    analyser.getByteFrequencyData(data)
+    draw(data)
+    requestAnimationFrame(animation)
+}
+
+requestAnimationFrame(animation)
 
 function clearSources() {
     for (const source of sources) {
         source.stop()
     }
     sources = []
+    if (additionalSource) {
+        additionalSource.stop()
+        additionalSource = undefined
+    }
 }
 
 function* sineWave(audioContext, frequency) {
@@ -26,7 +92,7 @@ function* sineWave(audioContext, frequency) {
     }
 }
 
-function playSound({ audioContext, array }) {
+function playSound({ audioContext, array, additional }) {
     const audioBuffer = audioContext.createBuffer(1, array.length, audioContext.sampleRate)
     audioBuffer.copyToChannel(array, 0)
     // clearSources()
@@ -35,7 +101,16 @@ function playSound({ audioContext, array }) {
     source.connect(gainNode)
     source.buffer = audioBuffer
     source.start()
-    sources.push(source)
+    if (additional) {
+        additionalSource = source
+    } else {
+        sources.push(source)
+    }
+
+    while (sources[polyphony]) {
+        sources[0].stop()
+        sources.shift()
+    }
 }
 
 function createButton(audioContext, hz) {
@@ -53,25 +128,26 @@ function createButton(audioContext, hz) {
 }
 
 function whiteNoise(audioContext) {
+    if (additionalSource) {
+        additionalSource.stop()
+        additionalSource = undefined
+        return
+    }
     const array = new Float32Array(audioContext.sampleRate).map(() => Math.random())
 
-    playSound({ array, audioContext })
+    playSound({ array, audioContext, additional: true })
 }
-
-let start = 80,
-    amount = 10,
-    gap = 40
 
 function createButtons(initial) {
     if (!initial) {
         gap = Math.max(gapEl.value, 1)
-        start = Math.min(Math.max(startEl.value, 1), 22000)
-        amount = Math.min(Math.max(amountEl.value, 1), 200)
+        start = Math.min(Math.max(startEl.value, 40), 22000)
+        polyphony = Math.min(Math.max(layersEl.value, 1), 100)
     }
 
     gapEl.value = gap
     startEl.value = start
-    amountEl.value = amount
+    layersEl.value = polyphony
 
     const target = document.getElementById('target')
     while (target.firstChild) {
@@ -82,6 +158,7 @@ function createButtons(initial) {
 
     let hz = start
     for (let i = 0; i < amount; i++) {
+        if (hz + i * gap > 22000) continue
         container.appendChild(createButton(audioContext, hz + i * gap))
     }
 
@@ -95,9 +172,13 @@ document.getElementById('off').addEventListener('click', () => clearSources())
 document.getElementById('reset').addEventListener('click', () => createButtons())
 
 document.addEventListener('keydown', (e) => {
-    if (e.target.nodeName !== 'BODY' || e.repeat) return
+    if (e.target.nodeName === 'INPUT' || e.repeat) return
     if (e.key === 'r' || e.key === 'R') {
         clearSources()
+        return
+    }
+    if (e.key === 'w' || e.key === 'W') {
+        whiteNoise(audioContext)
         return
     }
     let index = parseInt(e.key)
@@ -110,7 +191,7 @@ document.addEventListener('keydown', (e) => {
 })
 
 document.addEventListener('keyup', (e) => {
-    if (e.target.nodeName !== 'BODY' || e.repeat) return
+    if (e.target.nodeName === 'INPUT' || e.repeat) return
     let index = parseInt(e.key)
     if (isNaN(index)) return
     if (index === 0) index = 10
