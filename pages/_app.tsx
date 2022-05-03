3085d8cbb711796b917cc92555ac7201e7cf9f66
Arttu Pennanen <arttu@pennanen.org>
2022-05-03 13:45:16 +0300

    chore: analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { AppContextProvider } from '../lib/context'
 import { ThemeProvider } from 'next-themes'
+import { GoogleAnalytics, usePagesViews } from 'nextjs-google-analytics'
 
 function App({ Component, pageProps }: AppProps) {
+    usePagesViews()
     return (
         <ThemeProvider>
             <AppContextProvider>
+                <GoogleAnalytics />
                 <Component {...pageProps} />
             </AppContextProvider>
         </ThemeProvider>
