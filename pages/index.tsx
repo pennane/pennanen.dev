8ccaf29734732480da443efa156b0310a97fa9bf
Arttu Pennanen <arttu@pennanen.org>
2024-01-27 00:16:46 +0200

    feat: add an about me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import { generateMainPageImage } from '../lib/meta-image'
 import { getFilteredProjects } from '../lib/stuff'
 import { ProjectInterface } from '../types'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const description =
   'Arttu Pennanen - the digital playground pennanen.dev. Sharing my programming endeavours, from individual explorations to collaborative projects. Each piece is a step towards unraveling the vast world of code, crafted with desire to learn. Dive in to see where theory meets practice, through the lens of my experiences.'
@@ -34,11 +35,39 @@ const Index = ({
         <MainHeading />
       </header>
       <section>
-        <h2>Projects</h2>
+        <h2>About me</h2>
+        <div className="about">
+          <div>
+            <p>
+              Hello, I am Arttu. I like &quot;dumb&quot; stuff like functional
+              programming and using higher-order functions in JavaScript.
+            </p>
+            <p>
+              I&apos;m currently working as a Junior Software Engineer at
+              Hoxhunt and alongside completing an Information and Communication
+              Technology Bachelor&apos;s degree at Metropolia.
+            </p>
+            <p>
+              Here on my website, I post about the programming endeavors I find
+              myself tinkering with.
+            </p>
+          </div>
+          <aside>
+            <Image
+              alt='Black and white photo of myself giving thumbs-up outdoors."'
+              width={310}
+              height={200}
+              src={'/images/peukku.jpg'}
+            ></Image>
+          </aside>
+        </div>
+      </section>
+      <section>
+        <h2>Pinned projects</h2>
         <Projects projects={projectsData} />
       </section>
       <section className="all-link">
-        <Link href="/uncurated">Open list of uncurated projects</Link>
+        <Link href="/uncurated">List of uncurated projects</Link>
       </section>
     </Layout>
   )
