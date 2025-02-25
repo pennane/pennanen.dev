7f1ad55eb4b826a39fffdcdc459aa7ee112a0c88
Arttu Pennanen <arttu@pennanen.org>
2025-02-25 23:34:22 +0200

    add missing render fns

--- src/app/uncurated/page.tsx
+++ src/app/uncurated/page.tsx
@@ -2,13 +2,18 @@ import { getProjects } from '../[project]/lib'
 import { Stack } from '../../components/Stack'
 import styles from './page.module.css'
 import { ItemGroups } from '../../components/ProjectGroups'
+import { ProjectLink } from '../[project]/components/ProjectLink'
 
 export default function Page() {
   const projects = getProjects().filter((p) => p.ignoreInListing)
 
   return (
     <Stack className={styles.page}>
-      <ItemGroups items={projects} />
+      <ItemGroups
+        items={projects}
+        getDate={(x) => x.date}
+        render={(project) => <ProjectLink project={project} key={project.id} />}
+      />
     </Stack>
   )
 }
