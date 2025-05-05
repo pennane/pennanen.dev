80a718f7de5d34684540953717f114cfe48b16d7
Arttu Pennanen <arttu@pennanen.org>
2025-05-06 00:54:59 +0300

    center part 2

--- src/app/entries/components/BlogLink/index.tsx
+++ src/app/entries/components/BlogLink/index.tsx
@@ -5,7 +5,7 @@ import { Post } from '../../lib'
 
 export const BlogLink = ({ post }: { post: Post }) => {
   return (
-    <Stack horizontal alignItems="flex-start">
+    <Stack horizontal alignItems="center">
       <time>{formatDate(parseDateString(post.metadata.date))}</time>
       <Link href={`/entries/${post.slug}`} key={post.slug}>
         <h4>{post.metadata.title}</h4>
