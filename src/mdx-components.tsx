e8514dd5a9221c4f70defe429523ede4cfd39c40
Arttu Pennanen <arttu.pennanen@hoxhunt.com>
2025-06-06 20:55:50 +0300

    no new tab for same page links

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,13 @@ import { A } from './components/A'
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    a: (props) => <A rel="noopener noreferrer" target="_blank" {...props} />
+    a: (props) => {
+      const linkOnSamePage = props.href?.startsWith('#')
+      return linkOnSamePage ? (
+        <A {...props} />
+      ) : (
+        <A rel="noopener noreferrer" target="_blank" {...props} />
+      )
+    }
   }
 }
